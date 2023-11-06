933501864d0239a559c9b9bfd8164c3799a16b1f
Simon Zelenski <simon.zelenski@gmail.com>
2023-11-06 15:06:10 +0100

    Change logging to logger

--- node-net-manager/mqtt/MqttJobUpdates.go
+++ node-net-manager/mqtt/MqttJobUpdates.go
@@ -12,8 +12,10 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-var runningHandlers = utils.NewStringSlice()
-var runningHandlersLock sync.RWMutex
+var (
+	runningHandlers     = utils.NewStringSlice()
+	runningHandlersLock sync.RWMutex
+)
 
 type jobUpdatesTimer struct {
 	eventManager events.EventManager
@@ -35,7 +37,7 @@ type mqttInterestDeregisterRequest struct {
 }
 
 func (jut *jobUpdatesTimer) MessageHandler(client mqtt.Client, message mqtt.Message) {
-	log.Printf("Received job update regarding %s", message.Topic())
+	logger.InfoLogger().Printf("Received job update regarding %s", message.Topic())
 	go jut.env.RefreshServiceTable(jut.job)
 }
 
@@ -43,18 +45,18 @@ func (jut *jobUpdatesTimer) startSelfDestructTimeout() {
 	/*
 		If any worker still requires this job, reset timer. If in 5 minutes nobody needs this service, de-register the interest.
 	*/
-	log.Printf("self destruction timeout started for job %s", jut.job)
+	logger.InfoLogger().Printf("self destruction timeout started for job %s", jut.job)
 	eventManager := events.GetInstance()
 	eventChan, _ := eventManager.Register(events.TableQuery, jut.job)
-	for true {
+	for {
 		select {
 		case <-eventChan:
-			//event received, reset timer
+			// event received, reset timer
 			logger.DebugLogger().Printf("received packet event from: %s", jut.job)
 			continue
 		case <-time.After(10 * time.Second):
 			if !jut.env.IsServiceDeployed(jut.job) {
-				//timeout ----> job no longer required. Let's clear the interest
+				// timeout ----> job no longer required. Let's clear the interest
 				log.Printf("De-registering from %s", jut.job)
 				cleanInterestTowardsJob(jut.job)
 				jut.client.DeRegisterTopic(jut.topic)
@@ -75,11 +77,10 @@ func (jut *jobUpdatesTimer) startSelfDestructTimeout() {
 If the route is not used for more than 5 minutes the interest is removed
 If the instance number is provided, the interest is kept until that instance is deployed in the node */
 func MqttRegisterInterest(jobName string, env jobEnvironmentManagerActions, instance ...int) {
-
 	runningHandlersLock.Lock()
 	defer runningHandlersLock.Unlock()
 	if runningHandlers.Exists(jobName) {
-		log.Printf("Interest for job %s already registered", jobName)
+		logger.InfoLogger().Printf("Interest for job %s already registered", jobName)
 		return
 	}
 
@@ -98,7 +99,7 @@ func MqttRegisterInterest(jobName string, env jobEnvironmentManagerActions, inst
 
 	jobTimer.topic = "jobs/" + jobName + "/updates_available"
 	GetNetMqttClient().RegisterTopic(jobTimer.topic, jobTimer.MessageHandler)
-	log.Printf("MQTT - Subscribed to %s ", jobTimer.topic)
+	logger.InfoLogger().Printf("MQTT - Subscribed to %s ", jobTimer.topic)
 	runningHandlers.Add(jobTimer.job)
 	go jobTimer.startSelfDestructTimeout()
 }
